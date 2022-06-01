4e0a30e4f9aaec25109fbff9a3b7a99d31647ed4
Clément <cle.peaucelle@gmail.com>
2022-06-01 15:18:30 +0200

    test client

---
+++ client/src/pages/home/Home.jsx
@@ -0,0 +1,19 @@
+import Topbar from "../../components/topbar/Topbar1";
+import Sidebar from "../../components/sidebar/Sidebar1";
+import Feed from "../../components/feed/Feed";
+import Rightbar from "../../components/rightbar/Rightbar1";
+import "./home.css"
+
+export default function Home() {
+  
+  return (
+    <>
+      <Topbar />
+      <div className="homeContainer">
+        <Sidebar />
+        <Feed/>
+        <Rightbar/>
+      </div>
+    </>
+  );
+}
